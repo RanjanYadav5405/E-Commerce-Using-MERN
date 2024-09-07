97d642df5fbbe8012b913132b709af73e4574b2d
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-09-07 21:34:35 +0530

    backend product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -12,7 +12,7 @@ const productSchema = mongoose.Schema({
     timestamps : true
 })
 
-
+              
 const productModel = mongoose.model("product",productSchema)
 
 module.exports = productModel
